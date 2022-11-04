63462742cfb278114128b9e0af1c6b9af04db0fe
doggyB12 <90135567+doggyB12@users.noreply.github.com>
2022-11-05 01:04:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
-    introTitle: 'Nị ngu!',
+    introTitle: 'Nị ngu!!!',
     introDesc: `Bầu trời đêm nay không mây không sao
     trăng treo trên cao trong lòng anh vẫn nhớ nhung em nhiều
     Anh làm sao có thể ngừng suy nghĩ đến đôi môi em
     dù chỉ một giây`,
     btnIntro: 'hehe',
-    title: 'Dù cho mai về sau mình không bên cạnh nhau, làm tim anh quặn đau anh trông ngóng bao nhiêu lâu 😙',
+    title: 'Dù cho mai về sau mình không bên cạnh nhau, làm tim anh quặn đau anh trông ngóng bao nhiêu lâu 😘',
     desc: 'Dù vương vấn u sầu mùa thu có phai màu anh vẫn muốn yêu em ',
     btnYes: 'Yêu anh nhất <3',
-    btnNo: 'Xạo quá ông :3',
+    btnNo: 'Xạo quá ông :33',
     question:'Trên thế giới hơn 7 tỉ người mà anh lại yêu em <333',
     btnReply: 'Gửi cho anh <3',
     reply: 'Em thèm anh quá đi à <33333333',
